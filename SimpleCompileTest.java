b6dc66cf497b5e5dd0cfe734bf4ec1445ec1f316
Nathan Beal <nachtfalken@users.noreply.github.com>
2015-03-05 14:16:48 -0800

    Compiler Prototype
    The only it gets wrong is the files need to be in the same directory as the compiler at the moment

---
+++ SimpleCompileTest.java
@@ -0,0 +1,45 @@
+	
+	import javax.tools.*;
+	import java.io.*;
+	import java.util.*;
+	
+	public class SimpleCompileTest 
+	{
+		public static void main(String[] args) 
+		{
+			
+			try{
+				File[] files;
+				File dir = new File("Thing");
+				files = dir.listFiles(new FilenameFilter() {
+			    	public boolean accept(File dir, String name) {
+			        	return name.toLowerCase().endsWith(".java");
+			    	}
+				});
+				String[] filenames = new String[files.length];
+				for(int i = 0; i < files.length; i++)
+					filenames[i] = files[i].getName();
+	
+			File file = new File("Errors.txt");
+			try{
+				FileOutputStream errorStream = new FileOutputStream("Errors.txt");
+				
+				JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+					int compilationResult =	compiler.run(null, null, errorStream, filenames);
+							if(compilationResult == 0){
+								System.out.println("Compilation is successful");
+							}else{
+								System.out.println("Compilation Failed");
+							}
+			}catch(Exception e)
+			{
+				System.out.println("error in compiler");
+			}
+		}catch(Exception h)
+		{
+			System.out.println("error in filename");
+		}
+		}
+	
+	}
