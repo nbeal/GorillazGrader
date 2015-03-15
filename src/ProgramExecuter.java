8cf9cea8d0534f8ecac1cd52303e113535d60b41
Nathan Beal <nachtfalken@users.noreply.github.com>
2015-03-15 14:16:52 -0700

    Created object for running Programs

---
+++ src/ProgramExecuter.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
+public class ProgramExecuter
+{
+	private String directory;
+	private File dir;
+	
+	public ProgramExecuter(String filename)
+	{
+		directory = filename;
+		dir = new File(directory);
+	}
+		
+		public void runProgram()
+		{
+			try {
+				
+				Process p = Runtime.getRuntime().exec("java Tester", null, dir);
+				
+				BufferedReader in = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("java Tester", null, dir).getInputStream()));
+				
+				File file = new File(directory + ".txt");
+				FileOutputStream fis = new FileOutputStream(file);
+				PrintStream out = new PrintStream(fis);
+				System.setOut(out);  //Overrides System.out to file ("Assuming direct control")
+				
+				System.out.println("RUNNING TESTER IN" + directory + ". Results: \n");
+				String line = null;
+				while((line = in.readLine()) != null) {
+  					System.out.println(line);
+				}
+				System.out.println("\n\n--END OF LINE--\n"");
+				in.close();
+				out.close();
+				
+			}catch(Exception e)
+			{
+				System.out.println("error running");
+				//System.printStackTrace(;
+			}
+		}
+}
