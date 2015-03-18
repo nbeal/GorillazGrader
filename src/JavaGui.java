f61b304de9b538c6f5332a70e19600fa9fd67804
ewu-hollidayd <holliday.dylan@yahoo.com>
2015-03-18 10:17:34 -0700

    Update JavaGui.java

--- src/JavaGui.java
+++ src/JavaGui.java
@@ -1,26 +1,22 @@
-package cscd467;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import javax.swing.UIManager;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.List;
 
 import javax.swing.JFileChooser;
 import javax.swing.SpringLayout;
 import javax.swing.JTextArea;
-
-
 
 public class JavaGui {
 
@@ -29,6 +25,7 @@ public class JavaGui {
 	private JTextArea zipLocationJTextArea;
 	private JTextArea gradingJTextArea;
 	private JButton btnSubmit;
+	private String filename;
 
 	/**
 	 * Launch the application.
@@ -75,7 +72,7 @@ public class JavaGui {
 			public void actionPerformed(ActionEvent e) {
 				try{
 				
-					String filename = File.separator+"tmp";
+					filename = File.separator+"tmp";
 					JFileChooser fc = new JFileChooser(new File(filename));
 					// Show open dialog; this method does not return until the dialog is closed
 					int value = fc.showOpenDialog(frame);
@@ -83,15 +80,16 @@ public class JavaGui {
 					{
 						File file = fc.getSelectedFile();
 						pathToFile = file.getAbsolutePath();
-						if(isZipFile(file, pathToFile))
+						if(isZipFile(file, pathToFile)) //if Zip file
 						{
-						zipLocationJTextArea.setText(pathToFile);
-						btnSubmit.setEnabled(true);
+							zipLocationJTextArea.setText(pathToFile); //Show the user the path to the file
+							btnSubmit.setEnabled(true);	//allow submissions
 						}
-						else
+						else //Not a zip file
 						{
-						zipLocationJTextArea.setText("File must be of type zip");		
-						btnSubmit.setEnabled(false);
+							zipLocationJTextArea.setText(""); //reset path
+							JOptionPane.showMessageDialog(getFrame(), "File must be .zip");	//display message
+							btnSubmit.setEnabled(false); //disallow submissions
 						}
 					}
 				}
@@ -124,9 +122,12 @@ public class JavaGui {
 			public void actionPerformed(ActionEvent e) {
 				btnSubmit.setEnabled(false);
 				btnBrowse.setEnabled(false);
-				//NetClient netClient = new NetClient(pathToFile);
+				//Client netClient = new Client(filename, pathToFile);
 				//netClient.start();
-				gradingJTextArea.setText("Your score was: 100/100");
+				NetClient netClient = new NetClient(pathToFile);
+				netClient.start();
+				JOptionPane.showMessageDialog(getFrame(), ".zip successfully submitted");	
+				gradingJTextArea.setText("Your estimated score is: ");
 			}
 		});
 		springLayout.putConstraint(SpringLayout.WEST, btnSubmit, 0, SpringLayout.WEST, frame.getContentPane());
@@ -139,6 +140,11 @@ public class JavaGui {
 	    return filename.toLowerCase().endsWith(".zip");
 	  }
 	
-	}
+	
 
+	public JFrame getFrame()
+	{
+		return this.frame;
+	}
+}
 
